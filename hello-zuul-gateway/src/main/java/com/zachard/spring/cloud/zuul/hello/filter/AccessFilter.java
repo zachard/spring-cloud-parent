f8b9f1e9b7d5f721f9f57843413323de2b1c7458
zachard <arieszpzeng@yeah.net>
2018-01-09 14:55:52 +0800

    finish: Spring Cloud Zuul中拦截器异常处理设置RequestContext方式
    可参考内容:
    
    待完善之处:(1) 按照方式未成功实现

--- hello-zuul-gateway/src/main/java/com/zachard/spring/cloud/zuul/hello/filter/AccessFilter.java
+++ hello-zuul-gateway/src/main/java/com/zachard/spring/cloud/zuul/hello/filter/AccessFilter.java
@@ -17,10 +17,11 @@
 package com.zachard.spring.cloud.zuul.hello.filter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -43,11 +44,6 @@ public class AccessFilter extends ZuulFilter {
 	 * HTTP请求中对应的Token参数
 	 */
 	private static final String ACCESS_TOKEN = "accessToken";
-	
-	/**
-	 * 表示在路由之前执行过滤器
-	 */
-	private static final String PRE_FILTER_TYPE = "pre";
 
 	/**
 	 * 表示<code>ZuulFilter</code>的具体处理逻辑
@@ -66,9 +62,19 @@ public class AccessFilter extends ZuulFilter {
 			logger.error("请求token为空, 权限验证失败");
 			
 			// 设置Zuul路由响应及响应码
-			context.setSendZuulResponse(false);
-			context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
-			return null;
+//			context.setSendZuulResponse(false);
+//			context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+			
+			// 抛出异常, 并进入error过滤器, 并不会进入SendErrorFilter过滤器
+			try {
+				throw new RuntimeException("权限token为空");
+			} catch (Exception e) {
+				context.setThrowable(e);
+				context.set("sendErrorFilter.ran", true);
+				context.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				context.set("error.exception", e);
+			}
+			
 		}
 		
 		logger.info("权限认证通过, 请求token为: {}", accessToken);
@@ -99,13 +105,13 @@ public class AccessFilter extends ZuulFilter {
 	 * 表示过滤器的类型--过滤器在哪个生命周期内执行
 	 * 
 	 * @return  <code>pre</code>表示请求执行之前进行过滤, 
-	 *          <code>route</code>表示处理请求, 进行路由
+	 *          <code>route</code>表示请求执行时执行过滤器
 	 *          <code>post</code>表示请求处理完成之后执行过滤器, 
 	 *          <code>error</code>表示请求出现错误时执行过滤器
 	 */
 	@Override
 	public String filterType() {
-		return PRE_FILTER_TYPE;
+		return FilterConstants.PRE_TYPE;
 	}
 
 }
