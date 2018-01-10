2273744636599e1f344f63ca277bf6a358ea3ad2
zachard <arieszpzeng@yeah.net>
2018-01-10 23:14:24 +0800

    finish: Spring Cloud Config配置中心搭建起步
    可参考内容:
    
    待完善之处:

---
+++ hello-config-server/src/main/java/com/zachard/spring/config/server/hello/HelloConfigSeverApplication.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2015-2018 zachard, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.zachard.spring.config.server.hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <code>Spring</code>配置中心启动类
+ * 
+ * <pre>
+ *     (1) {@link EnableConfigServer}表示开启<code>Spring</code>配置中心功能
+ *     (2) {@link SpringBootApplication}注解相关作用见<code>Spring Boot</code>学习资料
+ * </pre>
+ *
+ * @author zachard
+ * @version 1.0.0
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class HelloConfigSeverApplication {
+	
+	/**
+	 * <code>Spring</code>配置中心服务端启动类
+	 * 
+	 * @param args    启动参数
+	 */
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HelloConfigSeverApplication.class).web(true).run(args);
+	}
+
+}
