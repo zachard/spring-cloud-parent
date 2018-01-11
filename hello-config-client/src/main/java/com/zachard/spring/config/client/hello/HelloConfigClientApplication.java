65cc3754bccb0aa9cb7a76fbbfd0c90c2a0c6579
zachard <arieszpzeng@yeah.net>
2018-01-11 23:17:20 +0800

    finish: Spring Cloud Config配置中心客户端获取服务端配置示例
    可参考内容: (1) @Value注解方式获取配置中心服务端属性示例
                (2) Environment环境对象获取配置中心服务端属性示例
    
    待完善之处:

---
+++ hello-config-client/src/main/java/com/zachard/spring/config/client/hello/HelloConfigClientApplication.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2015-2018 zachard, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.zachard.spring.config.client.hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * <code>Spring Config</code>配置中心客户端启动类
+ * 
+ * <pre>
+ *     (1) {@link SpringBootApplication}注解的作用见<code>Spring Boot</code>学习资料
+ * </pre>
+ *
+ * @author zachard
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class HelloConfigClientApplication {
+	
+	/**
+	 * <code>Spring Config</code>配置中心客户端启动方法
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HelloConfigClientApplication.class).web(true).run(args);
+	}
+
+}
