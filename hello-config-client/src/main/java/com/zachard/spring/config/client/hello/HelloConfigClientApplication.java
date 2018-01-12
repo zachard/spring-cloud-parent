769cee8dbe40f8b11e0ec90a671ab5227bc81896
zachard <arieszpzeng@yeah.net>
2018-01-12 16:47:41 +0800

    finish: Spring Cloud Config配置中心客户端注册到注册中心, 从而实现服务方式获取配置中心服务端配置
    可参考内容:
    
    待完善之处:

--- hello-config-client/src/main/java/com/zachard/spring/config/client/hello/HelloConfigClientApplication.java
+++ hello-config-client/src/main/java/com/zachard/spring/config/client/hello/HelloConfigClientApplication.java
@@ -18,17 +18,24 @@ package com.zachard.spring.config.client.hello;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * <code>Spring Config</code>配置中心客户端启动类
  * 
  * <pre>
  *     (1) {@link SpringBootApplication}注解的作用见<code>Spring Boot</code>学习资料
+ *     (2) {@link EnableDiscoveryClient}用于开启一个{@link DiscoveryClient}的实现
+ *         {@link DiscoveryClient}定义了一种通用的可以获取已经发现的服务的操作(开启发现服务的能力)
+ *         项目加入spring-cloud-starter-eureka架包并配置eureka.client.serviceUrl.defaultZone注册中心
+ *         地址之后, 不加入{@link EnableDiscoveryClient}似乎也没有影响
  * </pre>
  *
  * @author zachard
  * @version 1.0.0
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloConfigClientApplication {
 	
