bafa4de21d22c9a29fd24c804a328e397ef1ca70
zachard <arieszpzeng@yeah.net>
2018-01-12 16:36:12 +0800

    finish: Spring Cloud Config配置中心服务端注册到注册中心
    可参考内容:
    
    待完善之处:

--- hello-config-server/src/main/java/com/zachard/spring/config/server/hello/HelloConfigSeverApplication.java
+++ hello-config-server/src/main/java/com/zachard/spring/config/server/hello/HelloConfigSeverApplication.java
@@ -18,6 +18,8 @@ package com.zachard.spring.config.server.hello;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -26,11 +28,16 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * <pre>
  *     (1) {@link EnableConfigServer}表示开启<code>Spring</code>配置中心功能
  *     (2) {@link SpringBootApplication}注解相关作用见<code>Spring Boot</code>学习资料
+ *     (3) {@link EnableDiscoveryClient}用于开启一个{@link DiscoveryClient}的实现
+ *         {@link DiscoveryClient}定义了一种通用的可以获取已经发现的服务的操作(开启发现服务的能力)
+ *         项目加入spring-cloud-starter-eureka架包并配置eureka.client.serviceUrl.defaultZone注册中心
+ *         地址之后, 不加入{@link EnableDiscoveryClient}似乎也没有影响
  * </pre>
  *
  * @author zachard
  * @version 1.0.0
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class HelloConfigSeverApplication {
